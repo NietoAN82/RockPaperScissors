190956773c4dd5c6a4ac4eb0d7f142a041fcdda7
Alfredo Nieto <Nieto.Alfredo@outlook.com>
2023-05-09 20:04:38 -0500

    Initialize game, show the game score, and provide instructions.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,19 @@ namespace RockPaperScissors
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            int playerScore = 0;
+            int enemyScore = 0;
 
+            Console.WriteLine("Welcome to rock paper scissors!");
+
+            while(playerScore != 3 && enemyScore != 3)
+            {
+                Console.WriteLine("Player score - " + playerScore + ". Enemy score - " + enemyScore);
+                Console.WriteLine("Please enter 'r' for rock, 'p' for paper or anything else for scissors");
+                string playerChoice = Console.ReadLine();
+            }
+        }
     }
 }
